3daa9e99933e52d84615d1da6a9b7c210e9b8410
Sivapreetha <35474428+Sivapreetha@users.noreply.github.com>
2018-01-16 10:49:29 +0800

    Task completed

--- Exercise-Function-2.cpp
+++ Exercise-Function-2.cpp
@@ -1,5 +1,17 @@
-//this is an unfinish program
-//finish up this program
+//*Sivapreetha Anbumuthu
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+	int x=1,y=2;
+	int result;
+	
+	cout<<"x = "<<x<<" y = "<<y<<end1;
+	cout<<result<<x+y;
+	getch();
+}
 
 int sum(int x,int y)
 {
